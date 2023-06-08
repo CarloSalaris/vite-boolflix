69d152a78b7b33706eede30b421d85dc90f12c6c
CarloSalaris <carloa.salaris@gmail.com>
2023-06-08 11:50:44 +0200

    store.js API url + array for TV series

--- src/store.js
+++ src/store.js
@@ -3,5 +3,7 @@ import { reactive } from "vue";
 export const store = reactive({
     apiSearchMovieURL: "https://api.themoviedb.org/3/search/movie?api_key=63c693b8c2c00ea9447ca8a048c80455&query=",
     movieList: [],
+    apiSearchTvURL: "https://api.themoviedb.org/3/search/tv?api_key=63c693b8c2c00ea9447ca8a048c80455&query=",
+    tvList: [],
     searchText: "",
 });
