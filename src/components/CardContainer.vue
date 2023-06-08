6259b7c4f23df917a693b41bde83f61abc0d4cb5
CarloSalaris <carloa.salaris@gmail.com>
2023-06-08 16:36:54 +0200

    Milestone 3 Loading movie or Tv series image

--- src/components/CardContainer.vue
+++ src/components/CardContainer.vue
@@ -16,24 +16,26 @@ export default {
 </script>
 
 <template>
+    <!-- MOVIES SECTION -->
     <section v-if="store.movieList.length !== 0">
 
         <h2>MOVIES</h2>
 
         <div class="container">
             <!-- CARD -->
-            <SingleCard v-for="item in store.movieList" :key="item.id" :details="item" />
+            <SingleCard v-for="item in store.movieList" :key="item.id" :details="item" :imgRoot="store.apiImgBaseURL" />
         </div>
 
     </section>
 
+    <!-- TV SERIES SECTION -->
     <section v-if="store.tvList.length !== 0">
 
         <h2>TV SERIES</h2>
 
         <div class="container">
             <!-- CARD -->
-            <SingleCard v-for="item in store.tvList" :key="item.id" :details="item" />
+            <SingleCard v-for="item in store.tvList" :key="item.id" :details="item" :imgRoot="store.apiImgBaseURL" />
         </div>
 
     </section>
--- src/store.js
+++ src/store.js
@@ -6,4 +6,5 @@ export const store = reactive({
     apiSearchTvURL: "https://api.themoviedb.org/3/search/tv?api_key=63c693b8c2c00ea9447ca8a048c80455&query=",
     tvList: [],
     searchText: "",
+    apiImgBaseURL: "https://image.tmdb.org/t/p/w342"
 });
